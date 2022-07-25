a8fc38b30597c9e8357215eaeacb555db44926f5
Andrew <kaa2003.26@gmail.com>
2022-07-25 15:13:58 +0300

    first task done

--- lesson_1/task_1.py
+++ lesson_1/task_1.py
@@ -1,1 +1,19 @@
-print("Hello world")
+"""1. Реализовать вывод информации о промежутке времени в зависимости от его продолжительности duration в секундах:
+до минуты: <s> сек;
+* до часа: <m> мин <s> сек;
+* до суток: <h> час <m> мин <s> сек;
+* *до месяца, до года, больше года: по аналогии."""
+
+def convert_time(seconds):
+    days=seconds//86400
+    seconds=seconds %(24 *3600)
+    hour=seconds//3600
+    seconds%=3600
+    minutes = seconds // 60
+    seconds%=60
+    print(f"{days}:{hour}:{minutes}:{seconds}")
+convert_time(100000)
+
+    
+
+    
